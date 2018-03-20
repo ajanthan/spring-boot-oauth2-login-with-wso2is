fe74be56ef3877a68c412b9da7c37992929d287a
Ajanthan Balachandran <balaajanthan@gmail.com>
2018-03-20 13:56:01 -0700

    Generated skeleton project from start.spring.io

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spring-boot-oauth2-login-with-wso2is
 This a Spring Boot example with spring security OAuth2 login using WSO2 Identity server
+
+## Step 1
+
+Visit [https://start.spring.io](https://start.spring.com) and generate skeleton project by adding web,Security and Thymeleaf as dependencies.  
+![start.spring.io](images/start-spring-io.png?raw=true "Generate Spring Boot Project")
---
+++ src/main/java/com/github/ajanthan/spring/security/oauth2loginsample/Oauth2LoginSampleApplication.java
@@ -0,0 +1,12 @@
+package com.github.ajanthan.spring.security.oauth2loginsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2LoginSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2LoginSampleApplication.class, args);
+	}
+}
